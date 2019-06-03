cdd467095ba1be993c49b17705ee1e768b109622
mosastudio <mosastudio.io@gmail.com>
2019-06-03 20:03:07 +0800

    initial commit

---
+++ customview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customview
+//
+//  Created by Zick on 6/3/19.
+//  Copyright © 2019 mosastudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
