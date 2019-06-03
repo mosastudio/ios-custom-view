8a13c6c27a63428bca1231e2ead883c287fe3cbf
mosastudio <mosastudio.io@gmail.com>
2019-06-03 20:26:28 +0800

    custom UIView

---
+++ customview/ImageLabelView.swift
@@ -0,0 +1,21 @@
+//
+//  ImageLabelView.swift
+//  customview
+//
+//  Created by Zick on 6/3/19.
+//  Copyright © 2019 mosastudio. All rights reserved.
+//
+
+import UIKit
+
+class ImageLabelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- customview/ViewController.swift
+++ customview/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageLabelView: ImageLabelView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        imageLabelView.label.text = "Hello World"
     }
-
-
 }
 
