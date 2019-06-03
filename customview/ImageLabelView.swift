ba43afe0204fb90a97a7fbd9bb8588a781ca3bcd
mosastudio <mosastudio.io@gmail.com>
2019-06-03 20:26:47 +0800

    custom UIView

--- customview/ImageLabelView.swift
+++ customview/ImageLabelView.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class ImageLabelView: UIView {
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        Bundle.main.loadNibNamed("ImageLabelView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
 }
